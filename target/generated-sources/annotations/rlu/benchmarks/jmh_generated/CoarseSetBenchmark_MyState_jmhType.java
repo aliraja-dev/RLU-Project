b658a150d8cd6a83f98758fd44c0519193abd754
Ali Raja <aliraja.en@gmail.com>
2022-11-26 09:12:27 -0500

    clean debug start

--- target/generated-sources/annotations/rlu/benchmarks/jmh_generated/CoarseSetBenchmark_MyState_jmhType.java
+++
@@ -1,4 +0,0 @@
-package rlu.benchmarks.jmh_generated;
-public class CoarseSetBenchmark_MyState_jmhType extends CoarseSetBenchmark_MyState_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/rlu/benchmarks/jmh_generated/LockFreeSetBenchmark_MyState_jmhType.java
+++
@@ -1,4 +0,0 @@
-package rlu.benchmarks.jmh_generated;
-public class LockFreeSetBenchmark_MyState_jmhType extends LockFreeSetBenchmark_MyState_jmhType_B3 {
-}
-
